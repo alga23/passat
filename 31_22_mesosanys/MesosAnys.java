641b6d9ba2d65f7e305b7e378eea1031afd5c54e
prgtest <altronfut@gmail.com>
2022-10-25 10:26:35 +0200

    Pujat el 31_22  ∾ prgtest autocommit 31_22 #3

--- 31_22_mesosanys/MesosAnys.java
+++ 31_22_mesosanys/MesosAnys.java
@@ -9,32 +9,33 @@ public class MesosAnys {
 		System.out.println("Any?");
 		int any = Integer.parseInt(Entrada.readLine());  
 		
-		int mesAnterior = mes-1;
-		int mesSegüent = mes + 1;
-		
-		int anyAnterior = any -1;
-		int anySegüent = any +1;
-		
-		if (mes >=12) {
-			mesSegüent = 1;
-			any = anySegüent;
-			
+		if (mes == 1) {
+			int mesAnterior = 12;
+			mes = mes +1;
+		       int anyAnterior = any -1;
+			System.out.println("Mes anterior "  + mesAnterior + "/" + anyAnterior + " i mes següent " + mes + "/" + any);
 			}
-		else if (mes == 1) {
-			mesAnterior = 12;
-			any = anyAnterior;
-			
-			
-			
+		else if (mes >= 12) {
+			int mesSegüent = 1;
+			int anySegüent = any +1;
+			mes = mes -1;
+			System.out.println("Mes anterior "  + mes+ "/" + any + " i mes següent " + mesSegüent + "/" + any);
 			}
+		else {
 		
+			int mesAnterior = mes -1;
+			int mesSegüent = mes +1;
 			System.out.println("Mes anterior "  + mesAnterior + "/" + any + " i mes següent " + mesSegüent + "/" + any);
+		
+		
+	
 			
 			}
 		
 		
 		
 		}
+	}
 	
 		
 		
