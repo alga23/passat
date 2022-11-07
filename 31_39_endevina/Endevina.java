ee055b61366ae3e80517a766b5b86292ba96103c
alvaro garcia <altronfut@gmail.com>
2022-11-07 17:22:49 +0100

    Pujat el 31_39

---
+++ 31_39_endevina/Endevina.java
@@ -0,0 +1,37 @@
+/*Petit joc on l'usuari té que averiguar un nombre donat per el programa
+/.Li donarem diferents respostes depenent del número que li posi l'usuari
+/i el programa acabarà quan l'usuari ho endevini el nombre
+*/
+public class Endevina {
+	public static void main(String[] args) {
+		//posem els primer dos nombres
+		int nombreAEndevinar = 42;
+		int nombre = 0;
+		
+		while ( nombreAEndevinar != nombre ) {
+			System.out.println("Introdueix un valor");
+			nombre = Integer.parseInt(Entrada.readLine());
+	 		
+	 		//programen tots els possibles 
+	 		//camins que l'usuari pot tomar
+	 		
+	 		if (nombre < nombreAEndevinar) {
+	 			System.out.println("És més gran que " + nombre);
+	 			}
+	 		else if (nombre > nombreAEndevinar) {
+	 			System.out.println("És més petit que " + nombre);
+	 			}
+	 		else if (nombre == 0) {
+	 			System.out.println("Com a mínim 1");
+	 			}
+	 		else if (nombre > 100) {
+	 			System.out.println("Com a maxim 100");
+	 			}
+	 		else if (nombre == nombreAEndevinar) {
+	 			System.out.println("Has encertat");
+	 			}
+	 		}
+	 	}
+	 }
+	 			
+	 		
