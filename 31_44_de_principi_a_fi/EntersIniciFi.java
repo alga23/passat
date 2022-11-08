85daf9dcb07d385cd40f03aa624a282e78d86374
alvaro garcia <altronfut@gmail.com>
2022-11-08 16:14:15 +0100

    Pujat el 31_44

---
+++ 31_44_de_principi_a_fi/EntersIniciFi.java
@@ -0,0 +1,29 @@
+/*Pedirem tres nombres a l'usuari:
+/un inicial, un final i un salt.
+/El que farem és dir-li a l'usuari 
+/quants nombres de salt hi ha entre
+/l'inicial i el final
+*/
+public class EntersIniciFi {
+	public static void main (String[] args) {
+		//demanem l'inicial
+		
+		System.out.println("Valor inicial?");
+		int valorInicial =Integer.parseInt(Entrada.readLine());
+		//demanem el final
+		
+		System.out.println("Valor final?");
+		int valorFinal =Integer.parseInt(Entrada.readLine());
+		
+		//demanem el de salt
+				
+		System.out.println("Salt?");
+		int salt =Integer.parseInt(Entrada.readLine());
+		
+		while (valorInicial < valorFinal) {
+			System.out.println(valorInicial);
+			valorInicial = valorInicial +salt;
+			}
+		}
+	}
+		
