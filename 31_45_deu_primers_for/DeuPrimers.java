868cb1bd41b27876a8635988aebb88027969b205
alvaro garcia <altronfut@gmail.com>
2022-11-08 16:27:59 +0100

    Pujat el 31_45

---
+++ 31_45_deu_primers_for/DeuPrimers.java
@@ -0,0 +1,13 @@
+/*Tornarem a mostrar els primers 10 nombres
+/però aquesta vegada utilitzarem el bucle for 
+/en comptes del bucle while
+*/
+public class DeuPrimers {	
+	public static void main (String [] args) {
+		int numero = 10;
+		
+		for (int i = 1; i < numero; i++) {
+			System.out.println(i);
+			}
+		}
+	} 
