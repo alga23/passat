1795642e00fc5ac2bb96867144de2d1e855aa061
alvaro garcia <altronfut@gmail.com>
2022-11-08 18:20:13 +0100

    Pujat el 31_46

---
+++ 31_46_deu_primers_decreixent_for/DeuPrimersDecreixent.java
@@ -0,0 +1,13 @@
+/*Tornarem a mostrar els primers 10 nombres
+/però aquesta vegada utilitzarem el bucle for 
+/en comptes del bucle while
+*/
+public class DeuPrimersDecreixent {	
+	public static void main (String [] args) {
+		int numero = 1;
+		
+		for (int i = 10; i >= numero; i--) {
+			System.out.println(i);
+			}
+		}
+	} 
