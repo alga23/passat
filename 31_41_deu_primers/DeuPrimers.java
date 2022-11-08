85311cc2ebfbf59a0f21590c71ddfd9f654deb0e
alvaro garcia <altronfut@gmail.com>
2022-11-08 15:40:47 +0100

    Pujat el 31_41

---
+++ 31_41_deu_primers/DeuPrimers.java
@@ -0,0 +1,16 @@
+/*Aquest programa mostrarà els primers 10 nombres
+/(de l'1 al 10) utilitzant un bucle while
+*/
+public class DeuPrimers {
+	public static void main (String [] args) {
+		int numero = 1;         
+
+			while (numero <= 10) {  
+
+   		 		System.out.println(numero);
+
+   		 		numero = numero + 1;   
+
+}
+}
+}
