095b005d6ae6e4ae9727a21e47c1076f4e1f2ca1
alvaro garcia <altronfut@gmail.com>
2022-11-08 18:46:02 +0100

    Afegit el 31_47

---
+++ 31_47_primers_parells_for/PrimersParells.java
@@ -0,0 +1,15 @@
+/*Li demanem al programa que ens
+/mostri tots els nombres parells 
+/entre l'1 i el 10
+*/
+public class PrimersParells {
+	public static void main (String[] args) {
+		int numero = 10;
+		
+		for(int i = 2; i <= numero; i = i+ 2) {
+			System.out.println(i);
+			}
+		}
+	}
+ 
+
