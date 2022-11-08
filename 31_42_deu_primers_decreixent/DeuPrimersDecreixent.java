0c1ad404585c72decc943940ad81fb320df1604e
alvaro garcia <altronfut@gmail.com>
2022-11-08 15:52:10 +0100

    PUjat el 31_42

---
+++ 31_42_deu_primers_decreixent/DeuPrimersDecreixent.java
@@ -0,0 +1,16 @@
+/*Mostrarem els 10 primers nombres a l'inversa
+/(començant a partir del 10 i acabant a l'1)
+/utilitzant un bucle while 
+*/
+public class DeuPrimersDecreixent {
+	public static void main (String[] args){
+		int numero = 10;         
+			while (numero >= 1) {  
+
+    			System.out.println(numero);
+
+    			numero = numero - 1;    
+}
+
+}
+}
