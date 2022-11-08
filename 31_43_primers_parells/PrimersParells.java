9f3cbb754ed402ed53db091bc418d31d94edddd3
alvaro garcia <altronfut@gmail.com>
2022-11-08 16:01:39 +0100

    Pujat el 31_43

---
+++ 31_43_primers_parells/PrimersParells.java
@@ -0,0 +1,21 @@
+/*Aquest programa  mostra de més petit a més gran
+/tots els nombres parells que hi ha entre 1 i 10
+*/
+public class PrimersParells {	
+	public static void main (String[] args) {
+	
+	int numero = 2;        
+
+while (numero <= 10) {  
+	if (numero % 2 == 0) {
+	
+    System.out.println(numero);
+    }
+
+    numero = numero + 1;    
+    }
+
+}
+}
+
+		 
