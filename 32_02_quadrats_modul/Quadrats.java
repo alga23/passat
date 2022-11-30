5000860e7aa045a3bf7d1a955eaf11e10b530c32
alvaro garcia <altronfut@gmail.com>
2022-11-30 09:29:42 +0100

    PUjat el 32_02

--- 32_02_quadrats_modul/Quadrats.java
+++ 32_02_quadrats_modul/Quadrats.java
@@ -2,28 +2,40 @@
 /vegada utilitzarem un mòdul per optimitzar
 /el nostre programa
 */
-public class Quadrats{
+
+public class Quadrats {
 	public static void main (String[] args) {
 		System.out.println("Quants?");
-		int nombre =Integer.parseInt(Entrada.readLine());
-		 
-		 
-		 for (int linia=1; linia <= 5; linia++) {
+		int nombre = Integer.parseInt(Entrada.readLine()); 
+		dibuixaQuadrat(nombre);
+		}
+		
+		
+		
+		public static void dibuixaQuadrat(int quantitat) {
+		int cont = 0;
+		while(cont < quantitat){
+	
+		for (int linia=1; linia <= 5; linia++) {
 
             // dibuixa una línia
 
-            		for (int columna=1; columna <= 5; columna++) {
+           	 	for (int columna=1; columna <= 5; columna++) {
 
                 // dibuixa un element de la línia
+
+                System.out.print(" X");
 
             }
 
             // dibuixa un salt de línia
 
             System.out.println();
+           
 
         }
-
-    }
-
-}
+        System.out.println();
+        cont+= 1;
+       }
+      }
+     }
