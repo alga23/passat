e2535a8e227823aaaa7c325fa0b1e9b90bbb8efe
alvaro garcia <altronfut@gmail.com>
2022-12-01 18:38:51 +0100

    Afegit el 32_06

---
+++ 32_06_quadrat_reparametritzat/Quadrat.java
@@ -0,0 +1,21 @@
+/*NOva versió del quadrat on l'usuari
+/apart d'elegir el número d'elements
+/que compondrà el quadrat també podrà
+/dir quin caràcter serà aquest element
+*/
+public class Quadrat {	
+	public static void main (String[] args) {
+		int nombre = Integer.parseInt(args[0]);
+		String caracter = args[1];
+		dibuixaQuadrat(nombre, caracter);
+		
+		}
+	public static void dibuixaQuadrat(int nombre, String caracter){
+		for (int i = 1; i <= nombre; i++){
+			for (int j = 1; j <=nombre; j++){
+				System.out.print(caracter);
+				}
+			System.out.println("");
+			}
+		}
+	}
