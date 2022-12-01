cbdaed02b12ec5ea6646801b00aa86b9c89bc7f4
prgtest <altronfut@gmail.com>
2022-12-01 18:15:41 +0100

    Afegit el 32_04  ∾ prgtest autocommit 32_04 #1

--- 32_04_lloro_quadrat/LloroQuadrat.java
+++ 32_04_lloro_quadrat/LloroQuadrat.java
@@ -38,8 +38,8 @@ public class LloroQuadrat {
 		
 		
 		public static void dibuixaRectangle(){
-			for (int i = 1; i<=10; i++){
-				for (int j = 1; j<=5; j++){
+			for (int i = 1; i<=5; i++){
+				for (int j = 1; j<=10; j++){
 					System.out.print(" X");
 					}
 				System.out.println("");
