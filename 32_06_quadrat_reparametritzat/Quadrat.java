2a3f35d92435173b5b779e976746e6fd347c3abd
prgtest <altronfut@gmail.com>
2022-12-01 18:47:24 +0100

    Afegit el 32_06  ∾ prgtest autocommit 32_06 #2

--- 32_06_quadrat_reparametritzat/Quadrat.java
+++ 32_06_quadrat_reparametritzat/Quadrat.java
@@ -6,11 +6,12 @@
 public class Quadrat {	
 	public static void main (String[] args) {
 		int nombre = Integer.parseInt(args[0]);
-		String caracter = args[1];
+		String letra = args[1];
+		char caracter = letra.charAt(0);
 		dibuixaQuadrat(nombre, caracter);
 		
 		}
-	public static void dibuixaQuadrat(int nombre, String caracter){
+	public static void dibuixaQuadrat(int nombre, char caracter){
 		for (int i = 1; i <= nombre; i++){
 			for (int j = 1; j <=nombre; j++){
 				System.out.print(" " +caracter);
