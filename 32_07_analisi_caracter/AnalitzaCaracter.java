c05e3d1801fbbef383992a7693bb482b3df0eae9
prgtest <altronfut@gmail.com>
2022-12-16 17:23:15 +0100

    tORNAT A PUJAT EL 32_7  ∾ prgtest autocommit 32_07 #1

--- 32_07_analisi_caracter/AnalitzaCaracter.java
+++ 32_07_analisi_caracter/AnalitzaCaracter.java
@@ -9,23 +9,44 @@ public class AnalitzaCaracter{
 		System.out.println("Posició?");
 		int nombre = Integer.parseInt(Entrada.readLine());
 		if (nombre < 0) {
-			char caracterEscollit = cadena.charAt(nombre);
+		
+			if (nombre < cadena.length()) {
+				System.out.println("Fora de rang");
+				}
+			
+			else {
+				char caracterEscollit = 					cadena.charAt(cadena.length()-1);
+			
+				analitzaCaracter(caracterEscollit);
 			
 			}
+		}
 			
 			
 		
-		if (nombre > 0){
-		char caracterEscollit = cadena.charAt(nombre);
+		if (nombre >= 0){
+		
+			
+		
+			if (nombre > cadena.length()) {
+				System.out.println("Fora de rang");
+				}
 		
 		
-		analitzaCaracter(caracterEscollit, cadena);
+			else { 
+				char caracterEscollit = cadena.charAt(nombre);
+				analitzaCaracter(caracterEscollit);
+				}
 		}
 		
 		
 		
+			
+		
+		
+		
 		}
-	public static void analitzaCaracter(char caracter, String cadena) {
+	public static void analitzaCaracter(char caracter) {
 		
 		 if(Character.isDigit(caracter)){
 			System.out.println("'"+caracter+ "' és un nombre");
