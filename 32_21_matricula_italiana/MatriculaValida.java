ecb21ff100bdf759a17cb3d8a416b9337290456a
alvaro garcia <altronfut@gmail.com>
2022-12-23 19:44:59 +0100

    Afegit el 31_21

---
+++ 32_21_matricula_italiana/MatriculaValida.java
@@ -0,0 +1,42 @@
+/*En aquest programa, agafarem un text que 
+/revisarem per veure si és una matricula italiana
+/(correspon amb les lleis d'identificacio
+/que tenen aquestes
+*/
+public class MatriculaValida {
+	public static void main (String[] args) {
+		System.out.println("Introduïu una matrícula");
+		
+		String matricula = Entrada.readLine();
+		
+		esLletraValidaPerMatriculaItaliana(matricula);
+		
+		
+		}
+		
+	public static void esLletraValidaPerMatriculaItaliana(String matri){
+		for (int i = 0; i < matri.length(); i++) {
+			
+			if (Character.isLetter(matri.charAt(0))){
+				if (Character.isLetter(matri.charAt(1))){
+				
+					if (Character.isDigit(matri.charAt(2))){
+						if (Character.isDigit(matri.charAt(3))){
+							if (Character.isDigit(matri.charAt(4))){
+							
+								if (Character.isLetter(matri.charAt(5))){
+									if (Character.isLetter(matri.charAt(6))){
+										System.out.println("És una matrícula italiana vàlida");
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				else {
+					System.out.println("No és una matrícula italiana vàlida");
+					}
+				}
+			}
+		}
