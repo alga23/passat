c292e06ab29e08665b7b0b45725d99ac6bbaac2f
prgtest <altronfut@gmail.com>
2022-12-23 19:25:27 +0100

    Acabat el 32_19  ∾ prgtest autocommit 32_19 #1

--- 32_19_compta_vocals/ComptaVocals.java
+++ 32_19_compta_vocals/ComptaVocals.java
@@ -10,24 +10,70 @@ public class ComptaVocals {
         String entrada = Entrada.readLine();
 
         int numAs = quantesOcurrencies(entrada, 'a');
+        
+        int numAsAcent = quantesOcurrencies(entrada, 'à');
 
+        
+        
         int numEs = quantesOcurrencies(entrada, 'e');
+        
+        int numEsObert = quantesOcurrencies(entrada, 'è');
+        
+        int numEsTancat = quantesOcurrencies(entrada, 'é');
+        
+        
         
         int numIs = quantesOcurrencies(entrada, 'i');
         
+        int numIsTancat = quantesOcurrencies(entrada, 'í');
+        
+        int numIAmbDosPunts = quantesOcurrencies(entrada, 'ï');
+        
+        
+        
         int numOs = quantesOcurrencies(entrada, 'o');
         
+        int numOsTancat = quantesOcurrencies(entrada, 'ó');
+        
+        int numOsObert = quantesOcurrencies(entrada, 'ò');
+        
+        
+        
         int numUs = quantesOcurrencies(entrada, 'u');
+        
+        int numUsTancat = quantesOcurrencies(entrada, 'ú');
+        
+        int numUsAmbDosPunts = quantesOcurrencies(entrada, 'ü');
 
+        
+        
         mostraOcurrencies('a', numAs);
+        
+        mostraOcurrencies('à', numAsAcent);
 
         mostraOcurrencies('e', numEs);
         
+        mostraOcurrencies('è', numEsObert);
+        
+        mostraOcurrencies('é', numEsTancat);
+        
         mostraOcurrencies('i', numIs);
+        
+        mostraOcurrencies('í', numIsTancat);
+        
+        mostraOcurrencies('ï', numIAmbDosPunts);
                 
         mostraOcurrencies('o', numOs);
+        
+        mostraOcurrencies('ó', numOsTancat);
+        
+        mostraOcurrencies('ò', numOsObert);
                        
         mostraOcurrencies('u', numUs);
+        
+        mostraOcurrencies('ú', numUsTancat);
+        
+        mostraOcurrencies('ü', numUsAmbDosPunts);
     }
     public static void mostraOcurrencies(char lletra, int quantes) {
         System.out.println("Nombre de '" + lletra + "'s: " + quantes);
