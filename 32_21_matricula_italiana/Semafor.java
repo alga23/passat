4da0d0a1f66ace4679518f543b17f6d8346a3325
alvaro garcia <altronfut@gmail.com>
2022-12-24 10:38:26 +0100

    Pujat

---
+++ 32_21_matricula_italiana/Semafor.java
@@ -0,0 +1,34 @@
+/*Aquest programa preguntarà a l'usuari si és major
+/d'edat utilitzant la funció respoastaBoolean. En 
+/cas afirmatiu, li preguntarem per el color del
+/semafir i depenent del que ens digui, li donare 
+/una resposta o una altra
+*/
+public class Semafor {
+	public static void main (String[] args) {
+		System.out.println("Ets major d'edat?");
+		
+		String majorSioNo = Entrada.readLine();
+		
+		if (UtilitatsConfirmacio.respostaABoolean(majorSioNo == true){
+			System.out.println("Color?");
+			String color = Entrada.readLine();
+			colorSemafor(color);
+			}
+		else {
+			System.out.println("No pots fer servir aquest programa sense supervisió");
+			}
+		}
+		
+	public static void colorSemafor (String color) {
+		if(color.equals("groc"){
+			System.out.println("corre!");
+			}
+		else if (color.equals("vermell"){
+			System.out.println("espera");
+			}
+		else if(color.equals("verd"){
+			System.out.println("passa");
+			}
+		}
+	}
