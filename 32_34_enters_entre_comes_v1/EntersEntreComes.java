f235e6ff6f6ad0fd6588c613a06d8bfd4c9477c3
alvaro garcia <altronfut@gmail.com>
2023-01-02 17:31:53 +0100

    Pujat el 32_34

---
+++ 32_34_enters_entre_comes_v1/EntersEntreComes.java
@@ -0,0 +1,32 @@
+/*Aquest programa agafa un array
+/de tres elements i utilitzant un 
+/bucle hi posa una coma entre els tres
+*/
+
+public class EntersEntreComes {
+
+    public static void main(String[] args) {
+
+        int[] numeros;
+
+        numeros = new int[3];
+
+        numeros[0] = 1;
+
+        numeros[1] = 2;
+
+        numeros[2] = 3;
+        
+        System.out.print(numeros[0]);
+
+        for (int i = 1; i < numeros.length; i++) {
+
+            System.out.print(", " + numeros[i]);
+
+        }
+
+        System.out.println();
+
+    }
+
+}
