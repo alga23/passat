a7e1d45786d4dbb1a9ed8e7ceb568854928c3e15
alvaro garcia <altronfut@gmail.com>
2023-01-04 13:55:44 +0100

    Acabat el 32_23

--- 32_23_es_vocal/LloroVocalIniciFi.java
+++ 32_23_es_vocal/LloroVocalIniciFi.java
@@ -7,53 +7,50 @@
 */
 public class LloroVocalIniciFi {
 	public static void main (String[] args) {
-		System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");  
+		System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
 		String paraula = Entrada.readLine();
-		
-		invocaLloro(paraula);
-		
-		if (paraula.isEmpty()){
-			invocaLloro(paraula);
-			}
-			
-		
-	}
-	public static void invocaLloro(String paraula) {
-		
 			lloro(paraula);
 			
-			if (paraula.isEmpty()){
-				lloro(paraula);
-			}	
-			
-
 			}
 		
-	public static void lloro (String paraula) {
-	
-		if (UtilString.esVocal(paraula)) {
-			System.out.println("El lloro diu: " + paraula);
-			System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");  
-			paraula=Entrada.readLine();
-			if (!paraula.isEmpty()){
-			invocaLloro(paraula);
-			}
+		
+	public static void lloro(String paraula) {
+	if (paraula.isEmpty()) {
+			System.out.println("El lloro demana confirmació per finalitzar");
+			String finalitzar = Entrada.readLine();
 			
-			else {
-				System.out.println("El lloro demana confirmació per finalitzar");
-				
+			if (UtilitatsConfirmacio.respostaABoolean(finalitzar) == false) {
+				System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
 				paraula = Entrada.readLine();
-				
-				if(UtilitatsConfirmacio.respostaABoolean(paraula)){
-					System.out.println("Adéu");
-					}
-				else {
-					System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
-					invocaLloro(paraula);
-					}
+				lloro(paraula);
 				}
+			else if (UtilitatsConfirmacio.respostaABoolean(finalitzar) == true){
+			System.out.println("Adéu");
 			}
 		}
+		else if (UtilString.esVocal(paraula) == true) {
+			System.out.println("El loro diu: " + paraula);
+			System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
+			paraula = Entrada.readLine();
+			lloro(paraula);
+			}
+		else if (UtilString.esVocal(paraula) == false) {
+			System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
+			paraula = Entrada.readLine();
+			lloro(paraula);
+			}
+		
+		}
 	}
+
+				
+			
+			
+			 
+			
+
+	
+	
+	
 				
 		
