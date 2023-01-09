91cfd8c62bbf2897ee674ddaa43613ced135ced1
prgtest <altronfut@gmail.com>
2023-01-09 18:10:02 +0100

    Actualitzat  ∾ prgtest autocommit 32_08 #4

--- 32_08_mostra_interval_string/MostraInterval.java
+++ 32_08_mostra_interval_string/MostraInterval.java
@@ -8,50 +8,46 @@ public class MostraInterval {
 		System.out.println("text?");
 		String text =Entrada.readLine();
 		System.out.println("inici?");
-		int nombreInicial = Integer.parseInt(Entrada.readLine());
+		int nInici = Integer.parseInt(Entrada.readLine());
 		System.out.println("final?");
-		int nombreFinal = Integer.parseInt(Entrada.readLine());
+		int nFinal = Integer.parseInt(Entrada.readLine());
 		
-			mostraInterval(text, nombreInicial, nombreFinal);
+			mostraInterval(text, nInici, nFinal);
 		}
 		
-		public static void mostraInterval(String text, int nombreInicial, 							int nombreFinal) {
+		public static void mostraInterval(String text, int nInici, 							int nFinal) {
+			if (nInici < 0) {
+				nInici = 0;
+				}
+			if (nFinal < 0) {
+				nFinal = 0;
+				}
+			if (nInici > text.length()) {
+				nInici = 0;
+				}
+			if (nFinal > text.length()){
+				nFinal = 0;
+				}
+				
+			if (nInici < nFinal) {
+				for (int i = nInici; i < nFinal; i++){
+					char lletra = text.charAt(i);
+					System.out.println(lletra);
+					}
+			if (nFinal > nInici) {
+				for (int i = nFinal; i > nInici; i--){
+					char lletra = text.charAt(i);
+					System.out.println(lletra);
+					}
+				 
+				
 		
-		if (nombreInicial > nombreFinal) {
-			for(int i = nombreInicial; i >= nombreFinal; i--){
-				char lletra = text.charAt(i);
-				System.out.println(lletra);
-				}
-
-				
-			}
-				
-		
-		else if (nombreInicial < nombreFinal){
-		if (nombreFinal > text.length())
-			for (int i = nombreInicial; i < text.length(); i++){
-				char lletra = text.charAt(i);
-				System.out.println(lletra);
-				}
-			else if (nombreInicial < nombreFinal && nombreInicial < 0){
-				for (int i = 0; i < text.length() -1; i++){
-				char lletra = text.charAt(i);
-				System.out.println(lletra);
-				}
-			}
-				
-			else{
-			for (int i = 0; i < text.length() -1; i++){
-				char lletra = text.charAt(i);
-				System.out.println(lletra);
-				}			
-				}
-				
-
 		}
 		
 		}
 	}
+	
+}
 
 
 
