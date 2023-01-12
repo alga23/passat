351ab657d43378265a341e065a39664e76a60e60
alvaro garcia <altronfut@gmail.com>
2023-01-12 18:45:58 +0100

    Actulitzat el triangle de lletres

--- 32_10_triangle_lletres/TriangleLletres.java
+++ 32_10_triangle_lletres/TriangleLletres.java
@@ -5,33 +5,30 @@ public class TriangleLletres {
 	public static void main (String[] args) {
 		System.out.println("Text?");
 		String text = Entrada.readLine();
-		
-		//pedim text
-		
 		dibuixaTriangle(text);
+		
 		}
 		
-		
-		//creem modul
-		
-		public static void dibuixaTriangle(String textTriangle){
-			int linea = textTriangle.length();
-			//creem un int que té el mateix valor que nombre de lletres text
-			
-			dibuixaLinia(textTriangle, linea);
-			
+	public static void dibuixaTriangle(String text) {
+		for(int linia = 1; linia <= text.length(); linia++){
+			dibuixaLinia(text, linia);
+			System.out.println();
 			}
-			
-	      public static void dibuixaLinia(String textTriangle, int linia) {
-	      	 for (int i = 0; i < linia; i++) {
-        		String triangle = textTriangle.substring(0, i+1);
-        		System.out.println(", "+ triangle);
-	      		
-	      		 
-	      
-	      }
-	     }
-	    }
-	      	
+		
+		}
+		
+	public static void dibuixaLinia(String text, int linia) {
+		for (int i = 0; i < linia; i++) {
+			if (i + 1 == linia) {
+				System.out.print(text.charAt(i));
+			} else
+				System.out.print(text.charAt(i) + ", ");
+		}
+	}
+}
+
+		
+		
+	
 	      	
 			
