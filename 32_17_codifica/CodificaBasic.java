07c7935eba3b8e2c69e58fb1bbd9046554a7e6c1
alvaro garcia <altronfut@gmail.com>
2023-01-15 16:34:48 +0100

    ACtualitzat

---
+++ 32_17_codifica/CodificaBasic.java
@@ -0,0 +1,35 @@
+/*Aquest progrgramaagafarà un text 
+/ i farà una epecie d'encriptació
+/Cesar. EL nombre de lletres al
+/que pot saltar una letra i ser
+/substituida per el resultat
+/pot ser escollit per l'usuari
+*/
+public class CodificaBasic{
+	public static void main(String[] args) {
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		System.out.println("Quants?");
+		int quants = Integer.parseInt(Entrada.readLine());
+		if (quants < 0){
+			System.out.println("No s'accepten números negatius");
+		}
+		else{
+		
+			codifica(text, quants);
+		}
+	}
+	public static void codifica(String text, int quants) {
+		String textCodificat = "";
+		int nombreVocabulari = 26;
+		for(int i = 0; i < text.length(); i++){
+			char lletra =text.charAt(i);
+			int volta = lletra - 'a' + quants % nombreVocabulari + 'a';
+			char lletraCodificada= (char) volta;
+			textCodificat += lletraCodificada;
+			}
+		System.out.print(textCodificat);
+		}
+	}
+		
+	
