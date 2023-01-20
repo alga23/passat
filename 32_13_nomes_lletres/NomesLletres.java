c26aea7cf15a7a866f469625693088031edea95f
prgtest <altronfut@gmail.com>
2023-01-20 20:35:16 +0100

    Actualitzat per poder probar el prgtest en un exercici antic  ∾ prgtest autocommit 32_13 #2

--- 32_13_nomes_lletres/NomesLletres.java
+++ 32_13_nomes_lletres/NomesLletres.java
@@ -13,9 +13,13 @@ public class NomesLletres{
 		String fraseAmbComes = "";
 		for (int i = 0; i < text.length(); i++){
 			if (Character.isLetter(text.charAt(i))){
+				if(i + 1 > text.length()){
 				
-				fraseAmbComes = fraseAmbComes+text.charAt(i)+',';
+				fraseAmbComes = fraseAmbComes+text.charAt(i);
 				
+				}
+			else{
+				fraseAmbComes = fraseAmbComes+text.charAt(i) +  ", ";
 				}
 			
 			}
@@ -23,6 +27,7 @@ public class NomesLletres{
 
 			}
 		}
+	}
 		
 		
 	
