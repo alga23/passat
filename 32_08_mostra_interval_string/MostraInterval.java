f181d78ed74e4a137ccbd032689a0878e17ffd3a
alvaro garcia <altronfut@gmail.com>
2023-01-21 23:45:44 +0100

    Nous canvis

--- 32_08_mostra_interval_string/MostraInterval.java
+++ 32_08_mostra_interval_string/MostraInterval.java
@@ -9,55 +9,45 @@ public class MostraInterval {
 		String text =Entrada.readLine();
 		System.out.println("inici?");
 		int inici = Integer.parseInt(Entrada.readLine());
-		if (inici < 0){
-			inici = Math.abs(text.length()+inici);
-			}
+		
 		System.out.println("final?");
 		int nFinal = Integer.parseInt(Entrada.readLine());
 		
+		
+		if(nFinal > text.length()-1){
+      			nFinal = text.length()-1;
+    			}
+    		if(inici > text.length()-1){
+      			inici= text.length()-1;
+    }
+ 
+    mostraInterval(text, inici, nFinal);
+    }
+ 
+public static void mostraInterval(String text, int inici, int nFinal) {
+	  if(inici < 0) {
+	       inici = 0; 
+    }
+  	if(nFinal < 0) {
+    		nFinal = 0;}
+  		if(inici >= 0 || nFinal >= 0) {
+    			if (inici < nFinal) {
+     				 for (int i = inici; i <= nFinal; i++) {
+        System.out.println(text.charAt(i));   
+      }
+   }else if(inici >= nFinal) {
+      for(int i = inici; i >=nFinal; i++) {
+      System.out.println(text.charAt(i));
+      } 
+  }
+  }
+}
+}
 	
 		
-		mostraInterval(text, inici, nFinal);
-		}
+
 		
-	public static void mostraInterval(String text, int inici, int nFinal) {
-		if (inici < nFinal) {
-			if (nFinal > text.length()){
-				for(int i =inici; i < text.length(); i++){
-					System.out.println(text.charAt(i));
-				}
-			}
-			else if (inici < 0) {
-				for(int i =0; i < text.length(); i++){
-					System.out.println(text.charAt(i));
-					}
-				}
-				
-			else{
-				
-				for(int i =inici; i <= nFinal; i++){
-				System.out.println(text.charAt(i));
-				}
-			}
-		}
-			
-			
-		else if(inici > nFinal) {
-			
-      			for(int i = inici; i >= nFinal; i--) {
-      				System.out.println(text.charAt(i));
-			}
-				}
-				
-		else {
-			for(int i =0; i < text.length(); i++){
-					System.out.println(text.charAt(i));
-		
-		
-			}
-		}
-	}
-}
+	
 		
 
 			
