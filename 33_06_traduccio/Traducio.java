23ae60c712838ce47c1d893978bd08bdc9fa758e
alvaro garcia <altronfut@gmail.com>
2023-01-30 15:39:02 +0100

    Pujat tot per pode fer el 33_03

---
+++ 33_06_traduccio/Traducio.java
@@ -0,0 +1,8 @@
+/*Programa que agafa tres fitxers
+/en forma d'arguments i el que fem és substituir
+/certes praraules clau a un fitxer per altres
+*/
+public class Traducio{
+	public static void main(String[] args){
+		
+	
