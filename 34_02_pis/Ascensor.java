0c572995422866e362728471f88450e01b638f5a
alvaro garcia <altronfut@gmail.com>
2023-02-09 09:37:00 +0100

    Pujat el 34_02

---
+++ 34_02_pis/Ascensor.java
@@ -0,0 +1,12 @@
+/*Programa senzillet que simularà
+/un ascensor. Aquest ens dirà
+/solament la planta on es torba
+*/
+public class Ascensor{
+	public int pis = -1;
+		public static void main (String[] args){
+			Ascensor ascensor ;
+			ascensor = new Ascensor();
+			System.out.println("L'ascensor està a la planta " + ascensor.pis);
+			}
+		}
