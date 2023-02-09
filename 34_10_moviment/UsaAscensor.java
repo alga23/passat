8af52009ee437960ca222bfa3734145a76e46947
alvaro garcia <altronfut@gmail.com>
2023-02-09 11:49:23 +0100

    Pujat 34_10

---
+++ 34_10_moviment/UsaAscensor.java
@@ -0,0 +1,17 @@
+/*programa que utilitzarà
+/la classe ascensor per mostrar a l'usuari 
+/a quin pis està l'ascensor i else seus
+/moviments inicials i finals
+*/
+public class UsaAscensor{
+	public static void main(String [] args) {
+		Ascensor ascensor;
+		ascensor = new Ascensor();
+		System.out.println("Pis inicial:" + ascensor.pis);
+		System.out.println("Moviment inicial: " + ascensor.moviment);
+		if(ascensor.moviment.equals("aturat")){
+			ascensor.moviment = "pujant";
+			}
+		System.out.println("Moviment final: " + ascensor.moviment);
+		}
+	}	
