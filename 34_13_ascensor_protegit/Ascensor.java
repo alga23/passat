ff56cfbbb58731cf6b2a523916857ed692c0b6d9
alvaro garcia <altronfut@gmail.com>
2023-02-11 17:22:31 +0100

    PUjat 34_13

---
+++ 34_13_ascensor_protegit/Ascensor.java
@@ -0,0 +1,32 @@
+/*Classe AScensor que contè
+//els objectes per fer-la funcionar
+*/
+public class Ascensor {
+	private int pis = -1;
+	private String moviment = "aturat";
+	
+	public int getPis () {
+		return pis;
+		}
+	public void setPis (int nouPis) {
+		if (nouPis >= -1 && nouPis <=10) {
+			pis = nouPis;
+		}
+		else {
+			pis = pis;
+			}
+		}
+		
+	public String getMoviment () {
+		return moviment;
+		}
+	public void setMoviment (String nouMoviment) {
+		if (nouMoviment.equals("pujant") || nouMoviment.equals("baixant")){
+			moviment = nouMoviment;
+		}
+		else {
+			moviment = "aturat";
+		}
+	}
+}
+		
