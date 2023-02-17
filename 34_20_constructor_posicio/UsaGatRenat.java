512f47a9c2a926bb36bb69911257e5483fbf8aef
alvaro garcia <altronfut@gmail.com>
2023-02-17 15:19:01 +0100

    Pujat 34_20

---
+++ 34_20_constructor_posicio/UsaGatRenat.java
@@ -0,0 +1,9 @@
+/*Classe que utilitzara el GatREnat per dir quantes
+/vides té i en que posició es troba
+*/
+public class UsaGatRenat {
+	public static void main (String [] args) {
+		GatRenat renat = new GatRenat();
+		System.out.println(renat);
+		}
+	}
