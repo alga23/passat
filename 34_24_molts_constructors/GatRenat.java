f62bb741385ee6f5b7930dcbb9b0c0eed945f28d
alvaro garcia <altronfut@gmail.com>
2023-02-18 13:23:39 +0100

    Pujat el 34_24

---
+++ 34_24_molts_constructors/GatRenat.java
@@ -0,0 +1,53 @@
+/*Versió del GatRenat que conté diferents
+/constructors per al gat
+*/
+public class GatRenat {
+ 	  private int vides = 7;
+    	  private String posicio = "estirat";
+    	
+    	public GatRenat(int vides, String posicio) {
+        setVides(vides);
+        setPosicio(posicio);
+    }
+    
+    	public GatRenat () {
+    		this.getVides();
+    		this.getPosicio();
+    		}
+    	public GatRenat(int vides) {
+    		this.setVides(vides);
+    		}
+    	public GatRenat (String posicio){
+    		this.setPosicio(posicio);
+    		}
+    
+    public int getVides() { return vides; }
+    public String getPosicio() { return posicio; }
+    public void setVides(int novesVides) {
+        if (novesVides >= 0)  {
+            vides = novesVides;
+        }
+    }
+    public void setPosicio(String posicio){
+    	if (posicio.equals("dret")){
+    		this.posicio = posicio;
+    		}
+    	else {
+    		this.posicio = "estirat";
+    		}
+    	}
+    @Override
+    public String toString() { return String.format("Vides: %d. Posició: %s", vides, posicio); }
+    public static void main(String[] args) {
+    GatRenat[] renats = {
+        new GatRenat(),         // tot per defecte
+        new GatRenat(8),        // 8 vides i posició per defecte
+        new GatRenat("dret"),   // posició dret i vides per defecte
+        new GatRenat(8, "dret") // 8 vides i posició dret
+
+    };
+    for (GatRenat renat: renats) {
+        System.out.println(renat);
+    }
+}
+}
