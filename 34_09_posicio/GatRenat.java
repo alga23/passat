24cf8d1df60f6fe853533a7dccb205511f178fc5
alvaro garcia <altronfut@gmail.com>
2023-02-21 16:48:21 +0100

    Afegida una capçalera constructiva

--- 34_09_posicio/GatRenat.java
+++ 34_09_posicio/GatRenat.java
@@ -1,3 +1,7 @@
+/*Clase GatRenat que conté les vides 
+/i la posició d'aquest
+*/
+
 public class GatRenat {
     public int vides = 7;
     public String posicio = "estirat";         // vides disponibles del gat Renat
