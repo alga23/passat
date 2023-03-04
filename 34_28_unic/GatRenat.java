31c296f8e2b387cafbd25292f5631ed74cf2cae5
alvaro garcia <altronfut@gmail.com>
2023-03-04 17:25:45 +0100

    Pujat 34_28 altra vegada

--- 34_28_unic/GatRenat.java
+++ 34_28_unic/GatRenat.java
@@ -2,92 +2,106 @@
 /per a que solament només pugui 
 /agafar una instancia
 */
-public class GatRenat {
-	  private static GatRenat instancia;
- 	  private int vides = 7;
-    	  private String posicio = "estirat";
-    	
-    	private  GatRenat(int vides, String posicio) {
-        setVides(vides);
-        setPosicio(posicio);
-    }
+/*  Ejercicio que comprueba que valor asigna en la posicio del gat renat y en caso de que no sea correcta le dice que esta estirado
+    esta versión usa el 'this' para llamr a una variable con el mismo nombre, con muchos constructores*/
+import java.util.Objects;
+    public class GatRenat {
+        private int vides = 7;
+        private String posicio = "estirat";
+        private static GatRenat instancia = new GatRenat();
+
+        private GatRenat(int vides, String posicio) {
+            setVides(vides);
+            setPosicio(posicio);
+        }
+        private GatRenat(){
+            setPosicio("estirat");
+            setVides(7);
+        }
+        private GatRenat (String posicio) {
+            setPosicio(posicio);
+        }
+        private GatRenat (int vides) {
+            setVides(vides);
+        }
+        @Override
+        public String toString() {
+            return String.format("Vides: %d. Posició: %s", vides, posicio);
+        }
+        public int getVides() { return vides; }
+
+        public void setVides (int vides) {
+            if (vides >= 0) {
+                this.vides = vides;
+            } else {
+                this.vides = 7;
+            }
+        }
     
-    	private GatRenat () {
-    		this.getVides();
-    		this.getPosicio();
-    		}
-    	private GatRenat(int vides) {
-    		this.setVides(vides);
-    		}
-    	private GatRenat (String posicio){
-    		this.setPosicio(posicio);
-    		}
-    public static GatRenat getInstancia(){
-    	if (instancia == null) {
+        public String getPosicio() {return posicio;}
+    
+        public void setPosicio (String posicio) {
+            if (posicioCorrecta(posicio)) {
+                this.posicio = posicio; 
+            }
+            else {
+                this.posicio = "estirat";
+            }
+        }
+        //Comprova posicio correcta
+        public static boolean posicioCorrecta (String posicio) {
+            if(posicio.equals("dret") || posicio.equals("assegut") 
+            || posicio.equals("estirat")) return true; 
+            else return false;
+        }
+    
+        public static void main(String[] args) {
+            GatRenat[] renats = {
+                new GatRenat(),         // tot per defecte
+                new GatRenat(8),        // 8 vides i posició per defecte
+                new GatRenat("dret"),   // posició dret i vides per defecte
+                new GatRenat(8, "dret") // 8 vides i posició dret
+        
+            };
+            for (GatRenat renat: renats) {
+                System.out.println(renat);
+            }
+        }
+        public static GatRenat getInstancia () {
+            if (instancia == null) {
                 GatRenat instancia = new GatRenat();
                 return instancia; 
             } else {
                 return instancia;
             }
         }
-     public static GatRenat getInstancia(int vides){
-           if (instancia == null) {
-            	GatRenat instancia = new GatRenat(vides);
-        } else {
+        public static GatRenat getInstancia (int vides) {
+            if (instancia == null) {
+                GatRenat instancia = new GatRenat(vides);
+                return instancia;
+            } else {
                 instancia.setVides(vides);
                 return instancia;
             }
-        return instancia;
-    }
-     public static GatRenat getInstancia(String posicio){
-        if (instancia == null) {
-            instancia = new GatRenat(posicio);
-        } else {
+        }
+        public static GatRenat getInstancia (String posicio) {
+            if (instancia == null) {
+                GatRenat instancia = new GatRenat(posicio);
+                return instancia;
+            } else {
                 instancia.setPosicio(posicio);
                 return instancia;
             }
-        return instancia;
-    }
-     public static GatRenat getInstancia(int vides, String posicio){
-         if (instancia == null) {
-            instancia = new GatRenat(vides, posicio);
-        } else {
-        	instancia.setVides(vides);
+        }
+        public static GatRenat getInstancia (int vides, String posicio) {
+            if (instancia == null) {
+                GatRenat instancia = new GatRenat(vides,posicio);
+                return instancia;
+            } else {
+                instancia.setVides(vides);
                 instancia.setPosicio(posicio);
                 return instancia;
             }
-        return instancia;
-    }
-    public int getVides() { return vides; }
-    public String getPosicio() { return posicio; }
-    public void setVides(int novesVides) {
-        if (novesVides >= 0)  {
-            vides = novesVides;
         }
     }
-    public void setPosicio(String posicio){
-    	if (posicio.equals("dret")){
-    		this.posicio = posicio;
-    		}
-    	else if (posicio.equals("assegut")){
-    		this.posicio = "assegut";
-    		}
-    	else {
-    		this.posicio = "estirat";
-    		}
-    	}
-    @Override
-    public String toString() { return String.format("Vides: %d. Posició: %s", vides, posicio); }
-    public static void main(String[] args) {
-    GatRenat[] renats = {
-        new GatRenat(),         // tot per defecte
-        new GatRenat(8),        // 8 vides i posició per defecte
-        new GatRenat("dret"),   // posició dret i vides per defecte
-        new GatRenat(8, "dret") // 8 vides i posició dret
 
-    };
-    for (GatRenat renat: renats) {
-        System.out.println(renat);
-    }
-}
-}
