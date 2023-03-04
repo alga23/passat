debfec9293494b52a0839b39cef9607fcff645d0
alvaro garcia <altronfut@gmail.com>
2023-03-04 17:28:47 +0100

    Pujat 34_28 altra vegada

--- 34_28_unic/UsaGatRenat.java
+++ 34_28_unic/UsaGatRenat.java
@@ -3,25 +3,29 @@
 /una nova posició
 */ 
  public class UsaGatRenat {
-     public static void main(String[] args) {
-         GatRenat renat = GatRenat.getInstancia();
-         System.out.println("Inicialment Renat està " + renat.getPosicio());
-         for (String posicio: args) {
-             canviaPosicio(posicio);
-             System.out.println("Ara està " + renat.getPosicio());
-         }
-     }
-     private static void canviaPosicio(String novaPosicio) {
-	 String posicio = novaPosicio;
-    if (novaPosicio.equals("1") || novaPosicio.equals("estirat")) {
-        posicio = "estirat";
-    } else if (novaPosicio.equals("2") || novaPosicio.equals("assegut")) {
-        posicio = "assegut";
-    } else if (novaPosicio.equals("3") || novaPosicio.equals("dret")) {
-        posicio = "dret";
+     public class UsaGatRenat {
+    public static void main(String[] args) {
+        GatRenat renat = GatRenat.getInstancia();
+        System.out.println("Inicialment Renat està " + renat.getPosicio());
+        for (String posicio: args) {
+            canviaPosicio(posicio);
+            System.out.println("Ara està " + renat.getPosicio());
+        }
     }
-      GatRenat.getInstancia(novaPosicio);
+    private static void canviaPosicio(String novaPosicio) {
+        // Tradueix novaPosicio: 1 -> estirat, 2 -> assegut, 3 -> dret,
+        // altrament es queda com estava
+        if (novaPosicio.equals("1") || novaPosicio.equals("estirat")) {
+            novaPosicio = "estirat";
+        } else if (novaPosicio.equals("2") || novaPosicio.equals("assegut")) {
+            novaPosicio = "assegut";
+        } else if (novaPosicio.equals("3") || novaPosicio.equals("dret")) {
+            novaPosicio = "dret";
+        } else {
+            return;
+        }
+        GatRenat.getInstancia(novaPosicio);
+    }
+}
 
-          }
-         }
         
