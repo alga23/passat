cefaf7ae442ab6ef4e6eeb07b46aeb12b2e7724b
alvaro garcia <altronfut@gmail.com>
2023-03-04 21:36:33 +0100

    Afegit 34_25

--- 34_25_hora/Hora.java
+++ 34_25_hora/Hora.java
@@ -3,194 +3,199 @@
 /També incluirà els seus moduls
 /per poder canviar-la
 */
-public class  Hora {
-		private int hores = 0; 
-		private int minuts = 00;
-		private int segons = 00;
-		
-	public Hora(){
-		this.getHores();
-		this.getMinuts();
-		this.getSegons();
-		}
-	public Hora (int hores, int minuts, int segons){
-	if (hores < 23) {
-		setHores(hores);
-		}
-	if (minuts < 60) {
-		setMinuts(minuts);
-		}
-	if (segons < 60) {
-		setSegons(segons);
-		}
-	}
-	public int getHores () {return hores;}
-	
-	public int getMinuts () {return minuts;}
-	
-	public int getSegons () {return segons;}
-	
-	public void setHores (int hores){
-		if (hores >= 24 && hores < 0) {
-			this.hores = 0;
-			}
-		else {
-			this.hores = hores;
-			}
-		}
-	public void setMinuts (int minuts){
-		if (minuts >= 60 && minuts< 0) {
-			this.minuts = 0;
-			}
-		else {
-			this.minuts = minuts;
-			}
-		}
-		
-	public void setSegons(int segons){
-		if (segons >= 60 && segons < 0) {
-			this.segons = 0;
-			}
-		else {
-			this.segons = segons;
-			}
-		}
-	public void incrementa () {
-	    segons++;
-	    if (segons >= 60) {
-		segons = 0;
-		minuts++;
-		if (minuts >= 60) {
-		    minuts = 0;
-		    hores++;
-		    if (hores >= 24) {
-		        hores = 0;
-		    }
-		}
-	    }
-	}
+public class Hora {
+    private int hores = 0;
+    private int minuts = 0;
+    private int segons = 0;
 
-	public void incrementa (int incrementSegons) {
-	  if (incrementSegons >= 0) {
-		segons += incrementSegons;
-		while (segons >= 60) {
-		    segons -= 60;
-		    minuts++;
-		    if (minuts >= 60) {
-		        minuts = 0;
-		        hores++;
-		        if (hores >= 24) {
-		            hores = 0;
-		        }
-		    }
-		}
-	    } else {
-		while (incrementSegons < 0) {
-		    incrementSegons++;
-		    segons--;
-		    if (segons < 0) {
-		        segons = 59;
-		        minuts--;
-		        if (minuts < 0) {
-		            minuts = 59;
-		            hores--;
-		            if (hores < 0) {
-		                hores = 23;
-		            }
-		        }
-		    }
-		}
-	    }
-	}
-
-
-	
-	public void decrementa(int decrementSegons) {
-		   int totalSegons = hores * 3600 + minuts * 60 + segons;
-		    totalSegons -= decrementSegons;
-
-		    // Si el tiempo resultante es negativo, sumar 24 horas para obtener un tiempo válido
-		    if (totalSegons < 0) {
-			totalSegons += 86400;
-		    }
-
-		    hores = totalSegons / 3600;
-		    minuts = (totalSegons % 3600) / 60;
-		    segons = totalSegons % 60;
-		}
-
-
-	public void decrementa() {
-
-    if (segons > 0) {
-        segons--;
-    } else {
-        if (minuts > 0) {
-            minuts--;
-            segons = 59;
+    //Constructor de horas minutos y segundos
+    public Hora(int hores, int minuts, int segons) {
+        if(hores >=24 || hores <0 || minuts >= 60 || minuts < 0 ||segons >= 60 || segons < 0) {
+            setHores(0);
+            setMinuts(0);
+            setSegons(0);
         } else {
-            if (hores > 0) {
-                hores--;
-                minuts = 59;
-                segons = 59;
-            } else {
-                hores = 23;
-                minuts = 59;
-                segons = 59;
+            setHores(hores);
+            setMinuts(minuts);
+            setSegons(segons);
+        }
+    }
+    public Hora() {
+        setHores(0);
+        setMinuts(0);
+        setSegons(0);
+    }
+
+    // Getters
+    public int getHores() { return this.hores;}
+
+    public int getMinuts() { return this.minuts;}
+
+    public int getSegons() { return this.segons;}
+
+    // Setters
+    public void setHores(int hores) {
+        if (hores < 24 && hores >= 0) {
+            this.hores = hores;
+        }
+    }
+    public void setMinuts(int minuts) {
+        if (minuts < 60 && minuts >= 0) {
+            this.minuts = minuts;
+        }
+    }
+    public void setSegons (int segons) {
+        if (segons < 60 && segons >= 0) {
+            this.segons = segons;
+        }
+    }
+
+    // Incrementales
+    public void incrementa() {
+        if (segons + 1 < 60) {
+            setSegons(getSegons()+1);
+        } else if (segons + 1 == 60) {
+            setSegons(0);
+            if (minuts + 1 < 60) {
+                setMinuts(getMinuts()+1);
+            } else if (minuts + 1 == 60) {
+                setMinuts(0);
+                if (hores + 1 < 24) {
+                    setHores(getHores()+1);
+                } else if (hores + 1 >= 24) {
+                    setHores(0);
+                }
             }
         }
     }
-}
+    // Incrementa un numero de segundos que se le indica 
+    public void incrementa(int segons) {
+        int hora=0;
+        int minuts=0;
+        if (segons <  0) {
+            decrementa(Math.abs(segons));
+        } else {
+            if (segons >= 3600) {
+                hora = segons / 3600;
+                minuts = (segons / 60) - (hora * 60);
+                segons = segons - ((hora * 3600) + (minuts * 60));
+                incrementa(hora, minuts, segons);
+            } else if (segons < 3600 && segons > 59) {
+                hora = 0;
+                minuts = segons / 60;
+                segons = segons - (minuts * 60);
+                incrementa(hora, minuts, segons);
+            } else if (segons <=59 && segons >=0) {
+                hora = 0;
+                minuts = 0;
+                incrementa(hora, minuts, segons);
+            }
+        }
+    }
+    // hace los augmentos
+    public void incrementa(int hora, int minuts, int segons) {
+        if (getSegons() + segons >= 60) {
+            minuts++;
+            setSegons(getSegons()+segons-60);
+        } else {
+            setSegons(getSegons()+segons);
+        }
+        if (getMinuts() + minuts >= 60) {
+            hora++;
+            setMinuts(getMinuts()+minuts-60);
+        } else {
+            setMinuts(getMinuts() + minuts);
+        }
+        if (hora >= 24){
+            int numVueltas = hora/24;
+            hora = hora - numVueltas * 24;
+        }
+        if (getHores() + hora >= 24) {
+            setHores((getHores()-24) + hora);
+        } else {
+            setHores(getHores() + hora);
+        }      
+    }
 
+    // Decrementales
+    public void decrementa() {
+        if (getSegons() - 1 >= 0) {
+            setSegons(getSegons()-1);
+        } else if (getSegons() - 1 < 0) {
+            setSegons(59);
+            if (getMinuts() - 1 >= 0) {
+                setMinuts(getMinuts()-1);
+            } else if (getMinuts() - 1 < 0) {
+                setMinuts(59);
+                if (getHores() - 1 >= 0) {
+                    setHores(getHores()-1);
+                } else if (getHores() - 1 < 0) {
+                    setHores(23);
+                }
+            }
+        }
+    }
+    // Decrementa un numero de sugundos que se le indica
+    public void decrementa(int segons) {
+        int hora=0;
+        int minuts=0;
+        if (segons <  0) {
+            incrementa(Math.abs(segons));
+        } else {
+            if (segons >= 3600) {
+                hora = segons / 3600;
+                minuts = (segons / 60) - (hora * 60);
+                segons = segons - ((hora * 3600) + (minuts * 60));
+                decrementa(hora,minuts,segons);
+            } else if (segons < 3600 && segons > 59) {
+                hora = 0;
+                minuts = segons / 60;
+                segons = segons - (minuts * 60);
+                decrementa(hora, minuts, segons);
+            } else if (segons <=59 && segons >=0) {
+                hora = 0;
+                minuts = 0;
+                decrementa(hora, minuts, segons);
+            }
+        }
+    }
+    // Hace la resta de segundos que toque
+    public void decrementa(int hora, int minuts, int segons) {
+        if (hora > 24){
+            int numVueltas = hora/24;
+            hora = hora - numVueltas * 24;
+        }
+        if (getSegons() - segons < 0) {
+            minuts++;
+            setSegons(getSegons()+60-segons);
+        } else {
+            setSegons(getSegons()-segons);
+        }
+        if (getMinuts()- minuts < 0) {
+            hora++;
+            setMinuts(getMinuts()+60-minuts);
+        } else {
+            setMinuts(getMinuts()-minuts);
+        }
+        if (getHores()-hora < 0) {
+            setHores(getHores()+24-hora);
+        } else {
+            setHores(getHores()-hora);
+        }
+    }
+    public int compareTo( Hora hora2) {
+        if (getHores() == hora2.getHores() && getMinuts() == hora2.getMinuts() && getSegons() == hora2.getSegons()) return 0;
+        else if (getHores() == hora2.getHores() && getMinuts() == hora2.getMinuts() && getSegons() > hora2.getSegons()) return 1;
+        else if (getHores() == hora2.getHores() && getMinuts() > hora2.getMinuts()) return 1;
+        else if (getHores() > hora2.getHores()) return 1;
+        else return -1;
+    }
 
-	
-	public int compareTo (Hora hora2) {
-		if (this.hores > hora2.hores) {
-			return 1;
-			}
-		else if (this.hores == hora2.hores){
-			if (this.minuts > hora2.minuts){
-				return 1;
-				}
-				else if (this.minuts == hora2.minuts) {
-					if (this.segons > hora2.segons){
-						return 1;
-				}
-				}
-			}
-		
-		else if (this.minuts == hora2.minuts) {
-			if (this.segons > hora2.segons){
-				return 1;
-				}
-			}
-				
-		if (this.hores < hora2.hores) {
-			return -1;
-			}
-		else if (this.hores == hora2.hores){
-			if (this.minuts < hora2.minuts){
-				return -1;
-				}
-			else if (this.minuts == hora2.minuts) {
-				if (this.segons < hora2.segons){
-					return -1;
-					}
-				}
-				}
-			
-		else if (this.minuts == hora2.minuts) {
-			if (this.segons < hora2.segons){
-				return -1;
-				}
-			}
-		return 0;
-
-		}
-		
     @Override
-    public String toString() { return String.format("%d:%02d:%02d", hores, minuts, segons); }
-    
+    public String toString () {
+        return String.format("%d:%02d:%02d", hores, minuts, segons );
+    }
+
     /**
  * Compara dues hores i retorna l'operador corresponent
  * Per exemple, si hora1 és menor que hora2, l'operador serà "<". Els
@@ -225,8 +230,14 @@ public static void main(String[] args) {
             composaOperadorComparacio(hora1, hora2),
             hora2);
 
+    }
 }
-}
+	
+
+	
+	
+     
+	
     
 			
 
