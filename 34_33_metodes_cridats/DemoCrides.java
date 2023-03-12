ef1016507f3d2b4dc5579999196757b9f44bc080
alvaro garcia <altronfut@gmail.com>
2023-03-12 19:27:04 +0100

    Pujat el 34_33

---
+++ 34_33_metodes_cridats/DemoCrides.java
@@ -0,0 +1,79 @@
+/*Programa que experimenta amb les
+diferents sortides que es generen quan
+/tenim herencies de classes diferents
+*/
+
+class Feli {
+    public Feli() {
+        System.out.println("Neix Felí");
+    }
+    public void netejaUrpes() {
+        System.out.println("Felí neteja urpes des de " + this.getClass());
+    }
+    public void menja(Menjar menjar) {
+        System.out.println("Felí menja " + menjar + " des de " + this.getClass());
+    }
+}
+
+class Gat extends Feli {
+    public Gat() {
+        System.out.println("Neix Gat");
+    }
+    public void miola() {
+        System.out.println("Gat miola des de " + this.getClass());
+    }
+    @Override
+    public void menja(Menjar menjar) {
+        System.out.println("Gat menja " + menjar + " des de " + this.getClass());
+        super.menja(menjar);
+    }
+}
+
+class GatRenat extends Gat {
+    public GatRenat() {
+        System.out.println("Neix Renat");
+    }
+    @Override
+    public void miola() {
+        System.out.println("Renat miola des de " + this.getClass());
+    }
+
+    @Override
+    public void menja(Menjar menjar) {
+        System.out.println("Renat menja " + menjar + " des de " + this.getClass());
+        netejaUrpes();
+        super.menja(menjar);
+    }
+}
+
+class Menjar {
+    private static final String MENJAR_PER_DEFECTE = "sardines";
+    private final String nom;
+    public Menjar(String nom) {
+        if (nom != null && !nom.isEmpty()) {
+            this.nom = nom;
+        } else {
+            this.nom = MENJAR_PER_DEFECTE;
+        }
+        System.out.println("Neix Menjar(\"" + this.nom + "\")");
+    }
+    public String toString() { return nom; }
+}
+
+public class DemoCrides {
+
+public static void main (String [] args) {
+       Feli feli = new Feli();
+       Gat gat = new Gat();
+       GatRenat renat = new GatRenat();
+       feli.netejaUrpes();
+       gat.netejaUrpes();
+       renat.netejaUrpes();
+       gat.miola();
+       renat.miola();
+       Menjar menjar = new Menjar("bacallà");
+       renat.menja(menjar);
+
+
+ }
+}
