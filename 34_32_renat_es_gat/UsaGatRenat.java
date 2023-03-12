a1020b75e3ec435229b973b0fe9f75114db4e01d
alvaro garcia <altronfut@gmail.com>
2023-03-12 19:20:03 +0100

    Pujat el 34_32

---
+++ 34_32_renat_es_gat/UsaGatRenat.java
@@ -0,0 +1,12 @@
+/*Módul main on utilitzarem
+/el GatRenat que hem creat abans
+*/
+public class UsaGatRenat {
+   public static void main(String[] args) {
+     GatRenat renat = new GatRenat();
+     System.out.println("El Renat diu: "+ renat.aixecat());
+     System.out.println("El Renat diu: "+ renat.seu());
+     System.out.println("El Renat diu: "+ renat.estirat());
+     System.out.println("El Renat diu: "+ renat.estirat());
+  }
+}
