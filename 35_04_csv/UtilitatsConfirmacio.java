ab0a6fc09087feb30f75c44577486c5dc6497709
alvaro garcia <altronfut@gmail.com>
2023-04-09 22:40:03 +0200

    PUjat el 3504

---
+++ 35_04_csv/UtilitatsConfirmacio.java
@@ -0,0 +1,23 @@
+/*Programa que ens confirmarà
+/si el argument qu li hem passat 
+/sera true o false
+*/
+
+public class UtilitatsConfirmacio {
+public static boolean respostaABoolean(String resposta) {
+        if (null == resposta) {     // si la resposta és null, la donem com a false
+            return false;
+        }
+        resposta = resposta.toLowerCase();
+        if (resposta.equals("s") || resposta.equals("y")) {
+            return true;
+        }
+        if (resposta.equals("sí") || resposta.equals("yes")) {
+            return true;
+        }
+        if (resposta.equals("si") || resposta.equals("vale") || resposta.equals("yeah")) {
+            return true;
+        }
+        return false;
+    }
+}	
